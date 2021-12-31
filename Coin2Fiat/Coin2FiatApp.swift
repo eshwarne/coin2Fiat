43e9c41c37dee7a6736cbe33b245ccebe1d79449
eshwarsundar <northeasteditsofficial@gmail.com>
2021-12-31 12:37:31 +0530

    Initial Commit

---
+++ Coin2Fiat/Coin2FiatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin2FiatApp.swift
+//  Coin2Fiat
+//
+//  Created by Eshwar S on 31/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Coin2FiatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin2Fiat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coin2Fiat
+//
+//  Created by Eshwar S on 31/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
